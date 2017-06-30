c7a4e9534944a6a137141ce543bb915c9713e780
davidfang <fangmw@gmail.com>
2017-06-30 14:55:37 +0800

    devTools ok

--- App/Redux/CreateStore.js
+++ App/Redux/CreateStore.js
@@ -5,6 +5,8 @@ import createSagaMiddleware from 'redux-saga'
 import RehydrationServices from '../Services/RehydrationServices'
 import ReduxPersist from '../Config/ReduxPersist'
 import ScreenTracking from './ScreenTrackingMiddleware'
+import devTools from 'remote-redux-devtools';
+import { Platform } from 'react-native';
 
 // creates the store
 export default (rootReducer, rootSaga) => {
@@ -35,7 +37,11 @@ export default (rootReducer, rootSaga) => {
 
   // if Reactotron is enabled (default for __DEV__), we'll create the store through Reactotron
   const createAppropriateStore = Config.useReactotron ? console.tron.createStore : createStore
-  const store = createAppropriateStore(rootReducer, compose(...enhancers))
+  const store = createAppropriateStore(rootReducer, compose(...enhancers,devTools({
+    name: Platform.OS,
+    hostname: 'localhost',
+    port: 5678
+  })))
 
   // configure persistStore and check reducer version number
   if (ReduxPersist.active) {

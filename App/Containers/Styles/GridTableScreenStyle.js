722b017518491aaaccf3dd18d8f95edaaee404bb
davidfang <fangmw@gmail.com>
2017-07-12 12:19:05 +0800

    GridTableScreen

---
+++ App/Containers/Styles/GridTableScreenStyle.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native'
+import { ApplicationStyles, Metrics, Colors } from '../../Themes'
+
+export default StyleSheet.create({
+  ...ApplicationStyles.screen,
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background
+  },
+  row: {
+    width: Metrics.screenWidth / 2 - Metrics.doubleBaseMargin,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: Metrics.baseMargin,
+    backgroundColor: Colors.facebook,
+    borderRadius: Metrics.smallMargin
+  },
+  sectionHeader: {
+    // paddingTop: Metrics.doubleBaseMargin,
+    width: Metrics.screenWidth - Metrics.doubleBaseMargin,
+    height:Metrics.section,
+    alignSelf: 'center',
+    margin: Metrics.baseMargin,
+    backgroundColor: Colors.cloud
+  },
+  boldLabel: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    color: Colors.snow,
+    textAlign: 'center',
+    marginBottom: Metrics.smallMargin,
+    marginTop: Metrics.smallMargin
+  },
+  label: {
+    alignSelf: 'center',
+    color: Colors.snow,
+    textAlign: 'center'
+  },
+  listContent: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+})
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import GridTableScreen from '../Containers/GridTableScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  GridTableScreen: { screen: GridTableScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'GridTableScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
